57b95c6f0683496213665975c8f5b70c82fba57e
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2020-01-23 19:14:12 +0100

    Factory.TextGrid introduced

--- UWolframAlpha/UWolframAlpha.Data.UIElements.cs
+++ UWolframAlpha/UWolframAlpha.Data.UIElements.cs
@@ -51,6 +51,7 @@ namespace UWolframAlpha.Data
 		{
 			var ROOT = new VisualElement();
 			ROOT.Add( _Dev.TypeLabel(this) );
+			_Stylist.Box( ROOT );
 			{
 				var LABEL = new Label(title);
 				LABEL.SetEnabled( false );
@@ -58,7 +59,7 @@ namespace UWolframAlpha.Data
 
 				ROOT.Add( img.CreateVisualElement() );
 
-				var PLAINTEXT = _Factory.TextField( plaintext );
+				var PLAINTEXT = _Factory.TextGrid( plaintext );
 				ROOT.Add( PLAINTEXT );
 			}
 			return ROOT;
@@ -74,7 +75,7 @@ namespace UWolframAlpha.Data
 			{
 				// TODO: display image from src field
 
-				ROOT.Add( _Factory.TextField(src) );
+				ROOT.Add( _Factory.Text(src) );
 				// ROOT.Add( _Factory.TextField(alt) );
 				// ROOT.Add( _Factory.TextField(title) );
 			}
@@ -142,13 +143,61 @@ namespace UWolframAlpha.Data
 
 	static class _Factory
 	{
-		public static TextField TextField ( string text )
+		public static VisualElement Text ( string text )
 		{
-			if( text?.Length==0 ) return null;
-			var textField = new TextField();
-			textField.value = text;
-			textField.isReadOnly = true;
-			return textField;
+			if( text?.Length!=0 )
+			{
+				var FIELD = new TextField();
+				FIELD.value = text;
+				FIELD.isReadOnly = true;
+				return FIELD;
+			}
+			else return null;
+		}
+		public static VisualElement TextGrid ( string text )
+		{
+			if( text?.Length!=0 )
+			{
+				var ROOT = new VisualElement();
+
+				string[] rows = text.Split('\n');
+				foreach( string row in rows )
+				{
+					var ROW = new VisualElement();
+					ROW.style.flexDirection = FlexDirection.Row;
+					{
+						string[] cells = row.Split('|');
+						foreach( string cell in cells )
+						{
+							VisualElement CELL;
+							{
+								if( cell.Length!=1 )
+								{
+									CELL = new TextField();
+									((TextField)CELL).value = cell;
+									((TextField)CELL).isReadOnly = true;
+								}
+								else
+									CELL = new VisualElement();
+							}
+							{
+								var style = CELL.style;
+								style.flexBasis = 600f / (float)cells.Length;
+								style.flexShrink = 1f;
+								_Stylist.SetBorders( style , Color.grey , 1 );
+								_Stylist.SetPadding( style , 4 );
+								_Stylist.SetMargins( style , 0 );
+							}
+
+							ROW.Add( CELL );
+						}
+					}
+					ROOT.Add( ROW );
+				}
+
+				return ROOT;
+			}
+			else return null;
 		}
 	}
 
@@ -171,20 +220,20 @@ namespace UWolframAlpha.Data
 		public static void Box ( VisualElement ve )
 		{
 			var style = ve.style;
-			SetMargin( style , 5 );
+			SetMargins( style , 5 );
 			SetPadding( style , 5 );
-			SetBorder( style , Color.grey  , 1 );
+			SetBorders( style , Color.grey  , 1 );
 		}
 		public static void Label ( VisualElement ve )
 		{
 			var style = ve.style;
-			SetMargin( style , 5 );
+			SetMargins( style , 5 );
 			// SetPadding( style , 5 );
 			// SetBorder( style , Color.grey  , 1 );
 		}
-		public static void SetMargin ( IStyle style , int margin ) => style.marginTop = style.marginBottom = style.marginLeft = style.marginRight = margin;
+		public static void SetMargins ( IStyle style , int margin ) => style.marginTop = style.marginBottom = style.marginLeft = style.marginRight = margin;
 		public static void SetPadding ( IStyle style , int margin ) => style.paddingTop = style.paddingBottom = style.paddingLeft = style.paddingRight = margin;
-		public static void SetBorder ( IStyle style , Color color , int width )
+		public static void SetBorders ( IStyle style , Color color , int width )
 		{
 			style.borderTopWidth = style.borderBottomWidth = style.borderLeftWidth = style.borderRightWidth = width;
 			style.borderTopColor = style.borderBottomColor = style.borderLeftColor = style.borderRightColor = color;
