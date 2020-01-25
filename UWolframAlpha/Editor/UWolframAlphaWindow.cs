07c5e891c5d6be3646d27748b4f2204c97325677
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2020-01-25 13:57:41 +0100

    ROOT now in local scope only (nonsense nullcheck removed)

--- UWolframAlpha/Editor/UWolframAlphaWindow.cs
+++ UWolframAlpha/Editor/UWolframAlphaWindow.cs
@@ -10,107 +10,102 @@ namespace UWolframAlpha.Editor
 		const string k_editorprefskey_input = "UWolframAlphaWindow.input_value";
 		Color _color1 = new Color{ r=0.6f , g=0.6f , b=0.6f , a=1 };
 
-		VisualElement ROOT;
 		VisualElement OUTPUT;
 		TextField INPUT;
 
 		public void OnEnable ()
 		{
-			if( ROOT==null )
+			var ROOT = new VisualElement();
 			{
-				ROOT = new VisualElement();
-				{
-					var style = ROOT.style;
-					style.backgroundColor = Color.white;
-					style.flexGrow = 1f;
-				}
-				rootVisualElement.Add( ROOT );
-
-
-				var BAR = new VisualElement();
-				{
-					var style = BAR.style;
-					StyleMargin( style );
-					style.flexDirection = FlexDirection.Row;
-					style.minHeight = 30;
-				}
-				{
-					INPUT = new TextField();
-					{
-						INPUT.style.flexGrow = 1f;
-					}
-					{
-						INPUT.isDelayed = true;
-						INPUT.value = EditorPrefs.GetString( k_editorprefskey_input , "gold vs carbon vs iridium" );
-						INPUT.RegisterValueChangedCallback( (e) => {
-							EditorPrefs.SetString( k_editorprefskey_input , e.newValue );
-							OnButtonDown();
-						});
-					}
-					BAR.Add( INPUT );
-
-
-					var BUTTON = new Button( OnButtonDown );
-					{
-						BUTTON.style.flexGrow = 0.5f;
-					}
-					{
-						BUTTON.text = "=";
-					}
-					BAR.Add( BUTTON );
-				}
-				ROOT.Add( BAR );
-
-
-				var SCROLLVIEW = new ScrollView();
-				{
-					var style = SCROLLVIEW.style;
-					StyleMargin( style );
-					StyleBorder( style , _color1 );
-					style.flexGrow = 1f;
-				}
-				{
-					OUTPUT = new VisualElement();
-					SCROLLVIEW.Add( OUTPUT );
-				}
-				ROOT.Add( SCROLLVIEW );
-
-
-				var APPID_BAR = new VisualElement();
-				{
-					var style = APPID_BAR.style;
-					StyleMargin( APPID_BAR.style );
-					style.minHeight = 20;
-					style.flexDirection = FlexDirection.Row;
-				}
-				{
-					var APPID_FIELD = new TextField( 100 , false , true , '*' );
-					{
-						var style = APPID_FIELD.style;
-						style.flexGrow = 0.5f;
-					}
-					{
-						var LABEL = new Label("appid:");
-						APPID_BAR.Add( LABEL );
-
-						APPID_FIELD.value = EditorPrefs.GetString( k_editorprefskey_appid , string.Empty );
-						APPID_FIELD.RegisterValueChangedCallback( (e) => EditorPrefs.SetString( k_editorprefskey_appid , e.newValue ) );
-						APPID_FIELD.isDelayed = true;
-
-						LABEL.SetEnabled( false );
-					}
-					APPID_BAR.Add( APPID_FIELD );
-
-					var EMPTY = new VisualElement();
-					{
-						var style = EMPTY.style;
-						style.flexGrow = 1;
-					}
-					APPID_BAR.Add( EMPTY );
-				}
-				ROOT.Add( APPID_BAR );
-
+				var style = ROOT.style;
+				style.backgroundColor = Color.white;
+				style.flexGrow = 1f;
 			}
+			rootVisualElement.Add( ROOT );
+
+
+			var BAR = new VisualElement();
+			{
+				var style = BAR.style;
+				StyleMargin( style );
+				style.flexDirection = FlexDirection.Row;
+				style.minHeight = 30;
+			}
+			{
+				INPUT = new TextField();
+				{
+					INPUT.style.flexGrow = 1f;
+				}
+				{
+					INPUT.isDelayed = true;
+					INPUT.value = EditorPrefs.GetString( k_editorprefskey_input , "gold vs carbon vs iridium" );
+					INPUT.RegisterValueChangedCallback( (e) => {
+						EditorPrefs.SetString( k_editorprefskey_input , e.newValue );
+						OnButtonDown();
+					});
+				}
+				BAR.Add( INPUT );
+
+
+				var BUTTON = new Button( OnButtonDown );
+				{
+					BUTTON.style.flexGrow = 0.5f;
+				}
+				{
+					BUTTON.text = "=";
+				}
+				BAR.Add( BUTTON );
+			}
+			ROOT.Add( BAR );
+
+
+			var SCROLLVIEW = new ScrollView();
+			{
+				var style = SCROLLVIEW.style;
+				StyleMargin( style );
+				StyleBorder( style , _color1 );
+				style.flexGrow = 1f;
+			}
+			{
+				OUTPUT = new VisualElement();
+				SCROLLVIEW.Add( OUTPUT );
+			}
+			ROOT.Add( SCROLLVIEW );
+
+
+			var APPID_BAR = new VisualElement();
+			{
+				var style = APPID_BAR.style;
+				StyleMargin( APPID_BAR.style );
+				style.minHeight = 20;
+				style.flexDirection = FlexDirection.Row;
+			}
+			{
+				var APPID_FIELD = new TextField( 100 , false , true , '*' );
+				{
+					var style = APPID_FIELD.style;
+					style.flexGrow = 0.5f;
+				}
+				{
+					var LABEL = new Label("appid:");
+					APPID_BAR.Add( LABEL );
+
+					APPID_FIELD.value = EditorPrefs.GetString( k_editorprefskey_appid , string.Empty );
+					APPID_FIELD.RegisterValueChangedCallback( (e) => EditorPrefs.SetString( k_editorprefskey_appid , e.newValue ) );
+					APPID_FIELD.isDelayed = true;
+
+					LABEL.SetEnabled( false );
+				}
+				APPID_BAR.Add( APPID_FIELD );
+
+				var EMPTY = new VisualElement();
+				{
+					var style = EMPTY.style;
+					style.flexGrow = 1;
+				}
+				APPID_BAR.Add( EMPTY );
+			}
+			ROOT.Add( APPID_BAR );
 		}
 
 		async void OnButtonDown ()
