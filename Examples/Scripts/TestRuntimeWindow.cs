50e03da5d4488de4e0145b0f9ca846f0f9d79c6d
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2020-04-22 18:51:23 +0200

    working, runtime UIElements UWolfram window!

---
+++ Examples/Scripts/TestRuntimeWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Unity.UIElements.Runtime;
+
+namespace UWolframAlpha.Samples
+{
+	[RequireComponent(typeof(PanelRenderer))]
+	public class TestRuntimeWindow : MonoBehaviour
+	{
+
+		PanelRenderer _panelRenderer = null;
+
+		void Start ()
+		{
+			// magical pixie dust that makes ui appear (no idea why tho, might be a bug in early UIElements version)
+			_panelRenderer.enabled = false;
+			_panelRenderer.enabled = true;
+		}
+		
+		void OnEnable ()
+		{
+			_panelRenderer = GetComponent<PanelRenderer>();
+			_panelRenderer.postUxmlReload += BindPanelRenderer;
+		}
+
+		void OnDisable ()
+		{
+			_panelRenderer.postUxmlReload -= BindPanelRenderer;
+		}
+
+		IEnumerable<UnityEngine.Object> BindPanelRenderer ()
+		{
+			new UWolframAlpha.Window.Instance().Bind( _panelRenderer.visualTree );
+			return null;
+		}
+
+	}
+}
