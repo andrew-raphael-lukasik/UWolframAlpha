9c1558c6620f5223d69d1ad412fd96747e26a39e
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2020-09-13 19:17:49 +0200

    ported to 2020.1, obsolete components removed

---
+++ UWolframAlpha/Runtime/UWolframAlphaComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
+
+namespace UWolframAlpha.Runtime
+{
+	[RequireComponent( typeof(UIDocument) )]
+	public class UWolframAlphaComponent : MonoBehaviour
+	{
+		void Start ()
+		{
+			VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+			new UWolframAlpha.Window.Instance().Bind( root );
+		}
+	}
+}
